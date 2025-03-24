a9100d5087e7e5f3285cc4a9f1fbb3317003b9e1
MathiasPHA <Mathias.p.h.andersen@gmail.com>
2025-03-24 12:26:07 +0100

    Overworld scene change

--- P2 - Gruppe 5/Assets/Scripts/Overworld/Level_Connection.cs
+++ P2 - Gruppe 5/Assets/Scripts/Overworld/Level_Connection.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Levels/Connection")]
 public class Level_Connection : ScriptableObject
 {
-    
+    public static Level_Connection activeConnection { get; set; }
 }
