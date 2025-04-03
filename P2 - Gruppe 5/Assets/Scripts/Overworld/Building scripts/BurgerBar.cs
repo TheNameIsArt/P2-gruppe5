5587351454c2699ba3616a5cc8a7c007025c96fa
MathiasPHA <Mathias.p.h.andersen@gmail.com>
2025-04-03 12:32:06 +0200

    Interaction system

--- P2 - Gruppe 5/Assets/Scripts/Overworld/Building scripts/BurgerBar.cs
+++ P2 - Gruppe 5/Assets/Scripts/Overworld/Building scripts/BurgerBar.cs
@@ -5,17 +5,21 @@ public class BurgerBar : MonoBehaviour
 {
     
     private Animator animator;
+    public bool interactable;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        interactable = false;
         animator = GetComponent<Animator>();
         animator.Play("BurgerBar_Idle");
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (interactable)
+        {
+            animator.Play("BurgerBar_Interact");
+        }
     }
 }
