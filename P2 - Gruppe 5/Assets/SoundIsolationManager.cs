8f24824b9991fd0261ad5710c703c5fa2d8e7e8d
Marc5715 <marcus@juulnielsen.dk>
2025-04-04 15:00:15 +0200

    Update SoundIsolationManager.cs

--- P2 - Gruppe 5/Assets/SoundIsolationManager.cs
+++ P2 - Gruppe 5/Assets/SoundIsolationManager.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class SoundIsolationManager : MonoBehaviour
 {
+    [SerializeField] GameObject[] soundWaves;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        soundWaves = GameObject.FindGameObjectsWithTag("SoundWaves");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckSoundWaves();
+    }
+
+    void CheckSoundWaves()
+    {
+        bool allOff = true;
+        foreach (var soundWave in soundWaves)
+        {
+            if (soundWave.activeSelf)
+            {
+                allOff = false;
+                break;
+            }
+        }
+
+        if (allOff)
+        {
+            OnAllSoundWavesOff();
+        }
+    }
+
+    void OnAllSoundWavesOff()
+    {
+        // Add your logic here for when all sound waves are turned off
+        Debug.Log("All sound waves are turned off!");
+
     }
 }
