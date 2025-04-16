b73b07b7b3755c866a87c748a6515693845ea502
Viktor <viktor.bergstroem@hotmail.com>
2025-04-16 17:18:32 +0200

    Imported assets into Eating game

---
+++ P2 - Gruppe 5/Assets/Scripts/Eating Minigame/Eating GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatingGameManager : MonoBehaviour
+{
+    private List<string> foodSequence = new List<string>();
+    public SpriteRenderer[] foodSprites;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void GenerateNewFoodSequence()
+    {
+        
+    }
+}
---
+++ P2 - Gruppe 5/Assets/Scripts/Eating Minigame/Food Object.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FoodObject : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite defaultImage;
+    public Sprite pressedImage;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ButtonPress(InputAction.CallbackContext context)
+    {
+        Debug.Log("Input performed!");
+        if (context.performed)
+        {
+            spriteRenderer.sprite = pressedImage;
+        }
+    }
+
+    public void ButtonRelease(InputAction.CallbackContext context)
+    {
+        Debug.Log("Button released");
+        if (context.performed)
+        {
+            spriteRenderer.sprite = defaultImage;
+        }
+    }
+}
