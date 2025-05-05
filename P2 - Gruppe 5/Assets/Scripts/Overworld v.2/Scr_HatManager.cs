c91a2d9952453281723df55843c0331ce16c0eed
GustavNH <gustavnoerhaugesen@gmail.com>
2025-05-05 14:03:27 +0200

    Bluh

--- P2 - Gruppe 5/Assets/Scripts/Overworld v.2/Scr_HatManager.cs
+++ P2 - Gruppe 5/Assets/Scripts/Overworld v.2/Scr_HatManager.cs
@@ -10,12 +10,12 @@ public class Scr_HatSwitch : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
