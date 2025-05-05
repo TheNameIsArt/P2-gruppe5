bf9ca45cdac0d884836411d746be17b524baf97f
GustavNH <gustavnoerhaugesen@gmail.com>
2025-05-05 13:51:40 +0200

    BOT HAT

---
+++ P2 - Gruppe 5/Assets/Scripts/Overworld v.2/Scr_HatSwitch.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Scr_HatSwitch : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite[] Hats;
+
+    [SerializeField] private string hatChosen;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
