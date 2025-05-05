eec985cee23802ae862714c21b20badeb9a77666
GustavNH <gustavnoerhaugesen@gmail.com>
2025-05-05 19:53:31 +0200

    What's it like in new york city

--- P2 - Gruppe 5/Assets/Scripts/Overworld v.2/Scr_HatManager.cs
+++ P2 - Gruppe 5/Assets/Scripts/Overworld v.2/Scr_HatManager.cs
@@ -2,20 +2,76 @@ using UnityEngine;
 
 public class Scr_HatSwitch : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private SpriteRenderer hatRenderer;
     [SerializeField] private Sprite[] Hats;
 
-    [SerializeField] private string hatChosen;
+    [SerializeField] private string hatKey;
+
+    [SerializeField] private int hatChooser;
+    //public bool hatEnabled = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        hatRenderer = gameObject.GetComponent<SpriteRenderer>();
+        hatRenderer.enabled = false;
+        Debug.Log(Hats.Length);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        HatEnabler();
+        HatChooser();
+        HatDetector();
 
     }
+
+    void HatDetector()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && hatRenderer.enabled)
+        {
+            hatChooser = (hatChooser + 1) % Hats.Length-1;
+            Debug.Log(hatChooser);
+        }
+    }
+
+    void HatEnabler()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !hatRenderer.enabled)
+        {
+            hatRenderer.enabled = true;
+            hatChooser = 0;
+        }
+    }
+
+    void HatChooser()
+    {
+        switch (hatChooser)
+        {
+            case 0:
+                HatSelector(hatChooser);
+                hatKey = "Hat: " + hatChooser;
+                break;
+            case 1:
+                HatSelector(hatChooser);
+                hatKey = "Hat: " + hatChooser;
+                break;
+            case 2:
+                HatSelector(hatChooser);
+                hatKey = "Hat: " + hatChooser;
+                break;
+            default:
+                hatRenderer.enabled=false;
+                break;
+
+        }
+    }
+
+    void HatSelector(int hatChooser)
+    {
+        hatRenderer.sprite = Hats[hatChooser];
+    }
+
 }
