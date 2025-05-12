9b963b3b76c536daf442b5585ca140b0d677c8d6
ChristopherSHedegaard <chrissh02@gmail.com>
2025-05-12 13:00:43 +0200

    On Sceneload start Convo

--- P2 - Gruppe 5/Assets/WalkiTalkie.cs
+++ P2 - Gruppe 5/Assets/WalkiTalkie.cs
@@ -1,28 +1,17 @@
 using UnityEngine;
 using DialogueEditor;
-using System.Collections;
-
 public class WalkiTalkie : MonoBehaviour
 {
     public NPCConversation WalkiTalkiConvo;
-    private static bool hasEnteredScene = false;
-    [SerializeField] private GameObject convoZone; // Reference to the cat prefab
-
+    private static bool delilahHasEnteredScene = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (!hasEnteredScene)
+        if (!delilahHasEnteredScene)
         {
-            hasEnteredScene = true;
+            delilahHasEnteredScene = true;
             Debug.Log("This happens only the first time you enter the scene.");
             ConversationManager.Instance.StartConversation(WalkiTalkiConvo);
-            convoZone.SetActive(false); // Deactivate convo zone at the start
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
