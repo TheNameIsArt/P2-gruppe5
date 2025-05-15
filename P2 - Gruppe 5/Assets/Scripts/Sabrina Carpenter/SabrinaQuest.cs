0bee75616a80bce0daafa5ab9532a701111d3b6c
ChristopherSHedegaard <chrissh02@gmail.com>
2025-05-15 17:38:25 +0200

    Yeah

--- P2 - Gruppe 5/Assets/Scripts/Sabrina Carpenter/SabrinaQuest.cs
+++ P2 - Gruppe 5/Assets/Scripts/Sabrina Carpenter/SabrinaQuest.cs
@@ -7,9 +7,13 @@ public class SabrinaQuest : MonoBehaviour
 
     public static SabrinaQuest Instance { get; private set; }
     public bool sabrinaQuestActive = false;
+    private bool botTurnedOff = false;
+    public GameObject BandMember;
+
 
     private void Awake()
     {
+        BandMember = GameObject.FindGameObjectWithTag("BandMember");
 
         // If Sabrina Carpenter is Single, so am I
         if (Instance != null && Instance != this)
@@ -20,6 +24,20 @@ public class SabrinaQuest : MonoBehaviour
         Instance = this;
         // Uncomment the next line if you want the TaskManager to persist between scenes
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void Update()
+    {
+        if (!sabrinaQuestActive)
+        {
+            botTurnedOff = true;
+            BandMember.gameObject.SetActive(false);
+        }
+        else
+        {
+            botTurnedOff = false;
+            BandMember.gameObject.SetActive(true);
+        }
     }
 
 }
