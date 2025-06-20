243f1804f41e801bbc709d3df7b8d97f166f2449
ChristopherSHedegaard <chrissh02@gmail.com>
2025-06-20 11:08:52 +0200

    scenefader

--- P2 - Gruppe 5/Assets/Scripts/Overworld v.2/EndSceneScroller.cs
+++ P2 - Gruppe 5/Assets/Scripts/Overworld v.2/EndSceneScroller.cs
@@ -1,26 +1,58 @@
 using UnityEngine;
 
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
 public class EndSceneScroller : MonoBehaviour
 {
     public Rigidbody2D rb;
-    private float scrollSpeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float scrollSpeed = 1f;
+    public Image fadeOverlay;
+    public float fadeDuration = 2f;
+    public string sceneToLoad = "NextScene"; // Replace with your target scene name
+
+    private bool hasTriggered = false;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        scrollSpeed = 1;
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.linearVelocityY = scrollSpeed;
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "scrollStop") 
+        if (!hasTriggered)
         {
-            scrollSpeed = 0;
+            rb.linearVelocity = new Vector2(0, scrollSpeed);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!hasTriggered && other.CompareTag("scrollStop"))
+        {
+            hasTriggered = true;
+            rb.linearVelocity = Vector2.zero;
+            StartCoroutine(FadeAndLoadScene());
+        }
+    }
+
+    private IEnumerator FadeAndLoadScene()
+    {
+        float timer = 0f;
+        Color color = fadeOverlay.color;
+
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            float alpha = Mathf.Clamp01(timer / fadeDuration);
+            fadeOverlay.color = new Color(color.r, color.g, color.b, alpha);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
 }
+
