e7f6de09e0b3d762692d7f886b4eac80407894eb
cybence01 <viktor.bergstroem@hotmail.com>
2025-06-25 20:31:07 +0200

    Note Object script commented

--- P2 - Gruppe 5/Assets/Scripts/DJ Minigame/NoteObject.cs
+++ P2 - Gruppe 5/Assets/Scripts/DJ Minigame/NoteObject.cs
@@ -5,25 +5,23 @@ public class NoteObject : MonoBehaviour
 {
     public bool canBePressed;
     public InputAction notePressedAction; 
-    public GameObject goodHit, greatHit, perfectHit, missEffect;
+    public GameObject goodHit, greatHit, perfectHit, missEffect; // Prefabs for each hit effect
     private float missedPositionY = -2.25f;
-    private float pressWindowTime = 0.5f;
+    private float pressWindowTime = 0.5f; // The time that the note is presseable
     private float noteAppearY = 5f; //Y position where the note should be visible
     private bool hasAppeared = false;
     private SpriteRenderer spriteRenderer;
     public InputDevice inputDevice;
-    private float hitboxYPosition = -1.36f; 
-    private float goodHitYPosition = 0.25f;
-    private float greatHitYPosition = 0.10f;
+    private float hitboxYPosition = -1.36f; //This is where the note can be hit, according to the y-position
+    private float goodHitYPosition = 0.25f; //Distance from box which registers a good hit
+    private float greatHitYPosition = 0.10f; //Distance from box which registers a great hit
 
 
     private float timeWhenActivated; 
 
     void Start()
 {
-    timeWhenActivated = -1f;
-
-        //InputSystem.onActionChange += OnActionChange;
+    timeWhenActivated = -1f; //By making the time -1, the note is not being registered. 
 
     if (notePressedAction != null)
     {
@@ -31,10 +29,10 @@ public class NoteObject : MonoBehaviour
         notePressedAction.performed += ButtonPressed;
     }
 
-    spriteRenderer = GetComponent<SpriteRenderer>();
+    spriteRenderer = GetComponent<SpriteRenderer>(); // Finds the SpriteRenderer
     if (spriteRenderer != null)
     {
-        spriteRenderer.enabled = false; // hide at start
+        spriteRenderer.enabled = false; // hides notes at the start, saving process power.
     }
 }
 
@@ -46,18 +44,18 @@ public class NoteObject : MonoBehaviour
         hasAppeared = true;
         if (spriteRenderer != null)
         {
-            spriteRenderer.enabled = true;
+            spriteRenderer.enabled = true; // Shows the note once it enters the visible area
         }
     }
 
     // Missed note check
     if (transform.position.y < missedPositionY)
     {
-        if (timeWhenActivated >= 0 && Time.time - timeWhenActivated > pressWindowTime)
+        if (timeWhenActivated >= 0 && Time.time - timeWhenActivated > pressWindowTime) //If the note was activated and enough time passed, a miss is counted
         {
             DjGameManager.instance.NoteMissed();
             Instantiate(missEffect, transform.position, missEffect.transform.rotation);
-            gameObject.SetActive(false);
+            gameObject.SetActive(false); //Disables the note
         }
     }
 }
@@ -68,7 +66,7 @@ public class NoteObject : MonoBehaviour
         if (other.CompareTag("Activator"))
         {
             Debug.Log("Entered activator!");
-            canBePressed = true;
+            canBePressed = true; //Once the note enters the collider, it can be pressed
             timeWhenActivated = Time.time;
         }
     }
@@ -78,7 +76,7 @@ public class NoteObject : MonoBehaviour
     {
         if (other.CompareTag("Activator"))
         {
-            canBePressed = false;
+            canBePressed = false; //Once the note leaves the collider it can no longer be pressed
         }
     }
 
@@ -88,8 +86,9 @@ public class NoteObject : MonoBehaviour
 
         if (canBePressed)
         {
-            gameObject.SetActive(false);
+            gameObject.SetActive(false); //Deactivates the note after being pressed
 
+            //Based on the y-position from the hitbox, a hit effect is registered
             if (Mathf.Abs(transform.position.y - (hitboxYPosition)) > goodHitYPosition)
             {
                 Debug.Log("Good Hit!");
@@ -116,14 +115,4 @@ public class NoteObject : MonoBehaviour
         notePressedAction.performed -= ButtonPressed;
         notePressedAction.Disable();       
     }
-    /*private void OnActionChange(object obj, InputActionChange change)
-    {
-        if (change == InputActionChange.ActionPerformed)
-        {
-            var inputAction = (InputAction)obj;
-            var lastControl = inputAction.activeControl;
-            inputDevice = lastControl.device;
-        }
-
-    }*/
 }
